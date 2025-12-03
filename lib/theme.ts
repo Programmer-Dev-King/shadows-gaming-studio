c5e3b1fbc024652012b2e2681bf358e2f968a254
Nikhil <203668025+Programmer-Dev-King@users.noreply.github.com>
2025-12-03 18:32:08 +0530

    Create theme file with color constants and page themes

---
+++ lib/theme.ts
@@ -0,0 +1,24 @@
+const THEME = {
+    'shadow-black': '#000000',
+    'abyss-blue': '#0047AB',
+    'jin-woo-blue': '#4B0082',
+    'cursed-purple': '#6A0DC0',
+    'neon-cyan': '#00FFFF',
+    'crystal-green': '#00FF7F',
+    'chakra-orange': '#FFA500',
+    'silver-white': '#C0C0C0',
+    'icy-blue': '#E0FFFF'
+};
+
+const PAGE_THEMES = {
+    home: THEME['shadow-black'],
+    about: THEME['abyss-blue'],
+    services: THEME['jin-woo-blue'],
+    contact: THEME['cursed-purple'],
+    blog: THEME['neon-cyan'],
+    gallery: THEME['crystal-green'],
+    shop: THEME['chakra-orange'],
+    help: THEME['silver-white']
+};
+
+export { THEME, PAGE_THEMES };
